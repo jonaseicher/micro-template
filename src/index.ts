ac62780deb2dd11e267b3c4c9153a2fc6987923b
Jonas Eicher <jonas.maximilian.eicher@gmail.com>
2022-12-19 14:23:58 +0100

    add tsconfig, prettier

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import axios from 'axios';
+
+console.log('hello world');
+
+const app = express();
+
+app.get('/info', (_req, res) => {
+  res.send('hello world');
+});
+
+app.get('/axios', async (_req, res) => {
+  const response = await axios.get('http://localhost:8080/info');
+  res.send(response.data);
+});
+
+app.listen('8080');
